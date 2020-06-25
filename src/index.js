54de1f7b1fe493c1fb3a9a80a76630ce7636bb1e
luisyge35 <luisys97@gmail.com>
2020-06-25 16:21:14 +0200

    Previo definicion de estructura de API

--- src/index.js
+++ src/index.js
@@ -1,47 +1,10 @@
-var http = require('http');
-var {info,error} = require('./modules/my-log')
-var {countries} = require('countries-list')
-var url = require('url')
-var querystring = require('querystring')
-var server = http.createServer(function(request,response){
-    var parsed = url.parse(request.url)
-    var pathname = parsed.pathname; 
-    console.log('parsed :',parsed)
-    var query = querystring.parse(parsed.query)
-    console.log('query:', query)  
-    if (pathname === '/'){
-        response.writeHead(200,{'Content-Type':'text/html'});
-        response.write('<html><body><p>HOME PAGE</p></body></html>');
-        response.end();
-    } else if (pathname === '/exit'){
-        response.writeHead(200,{'Content-Type':'text/html'});
-        response.write('<html><body><p>EXIT PAGE</p></body></html>');
-        response.end();
-    } else if(pathname === '/info'){
-        var result = info(pathname)
-        response.writeHead(202,{'Content-Type':'text/html'});
-        response.write(result);
-        response.end();
-    }
-    else if (pathname === '/error'){
-        var result = error(pathname)
-        response.writeHead(202,{'Content-Type':'text/html'});
-        response.write(result);
-        response.end();
-    }
-    else if (pathname === '/country'){
-        var result = error(pathname)
-        response.writeHead(202,{'Content-Type':'application/json'});
-        response.write(JSON.stringify(countries[query['code']]));            
-        response.end();
-    } else{
-        response.writeHead(404,{'Content-Type':'text/html'});
-        response.write('<html><body><p>NOT FOUND</p></body></html>');
-        response.end();
-    }
+const express = require('express');
+const { routes } = require('./routes/index');
 
+const app = express();
 
-})
+routes(app);
 
-server.listen(4000);
-console.log('running on 4000')
+app.listen(4000, () => {
+  console.log('running on 4000');
+});
--- src/modules/my-log.js
+++ src/modules/my-log.js
@@ -1,12 +1,13 @@
-function info(text){
+const info = text => {
     console.log('INFO:', text);
     return text;
 }
 
-module.exports.error = function error(text){
-    console.log('ERROR:',text);
+const error = text => {
+    console.log('ERROR:', text);
     return text;
 }
 
 module.exports.info = info;
+module.exports.error = error;
 //module.exports = {info,error};
