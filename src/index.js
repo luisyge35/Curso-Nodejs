d93cdc9a7543800cd2a5747f822e3fdbed83e79d
luisyge35 <luisys97@gmail.com>
2020-06-26 19:49:29 +0200

    Ya conectado a mongo

--- src/index.js
+++ src/index.js
@@ -1,11 +1,28 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+const mongoose = require('moongoose');
 const routesV1 = require('./routes/v1/index');
+
+dotenv.config();
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+console.log('MONGO: ', process.env.MONGO);
 routesV1(app);
-app.listen(4000, () => {
-  console.log('running on 4000');
+const PORT  = process.env.PORT || 4000;
+
+mongoose.connect(process.env.MONGO, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => {
+  console.log('Connected to MongoDB');
+  app.listen(PORT, () => {
+    console.log(`running on: ${PORT}`);
+  });
+}).catch(error => {
+  console.log('MongoDB error: ', error);
 });
+
+
